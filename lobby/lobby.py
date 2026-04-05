f359d2ae2bab15c273aface65aef82799cc52996
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2026-04-05 01:51:32 -0300

    implementando a fase 2: o lobby

---
+++ lobby/lobby.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from threading import Lock
+from uuid import uuid4
+import socket
+import json
+
+@dataclass
+class Player:
+    name: str
+    ip: str
+    socket: socket.socket
+     
+     
+players: dict[str, Player] = {}
+
+players_lock = Lock()
+
+def enter_lobby(name: str, ip: str, conn: socket):
+    
+    player_id = str(uuid4())
+    
+    with players_lock:
+        players[player_id] = Player(name=name, ip=ip, socket=conn)        
+        
+    return player_id
+
+def leave_lobby(player_id: str):
+    
+    with players_lock:
+        players.pop(player_id, None)
+        
+def broadcast_lobby():
+    
+    players_in_lobby = [
+        {
+            "id": player_id,
+            "nome": players[player_id].name
+        } 
+        for player_id in players]
+    
+    players_in_lobby_json = json.dumps(players_in_lobby)+"\n"
+    
+    players_in_lobby_bytes = players_in_lobby_json.encode()
+    
+    for player in players_in_lobby:
+        
+        players[player["id"]].socket.sendall(players_in_lobby_bytes)
--- server/tcp_client.py
+++ server/tcp_client.py
@@ -1,25 +1,46 @@
 from concurrent.futures import ThreadPoolExecutor
+from ..lobby.lobby import enter_lobby, leave_lobby, broadcast_lobby
 import socket
 import json
 
-def handle_connection(conn):
+def handle_connection(conn, addr):
     
     buffer = b""
     message = []
+    
+    player_id = None
+    
     while True:
-        data = conn.recv(1024)
+        try:
+            data = conn.recv(1024)
+        except (ConnectionResetError, BrokenPipeError):
+            if player_id:
+                leave_lobby(player_id=player_id)
+                broadcast_lobby()
+            break
+            
         if not data:
             conn.close()
             break
         buffer += data
         before, sep, after = buffer.partition(b"\n")
         
-
-        
         if sep:
             before_as_dict = json.loads(before)
             print(before_as_dict)
             
+            if before_as_dict["acao"] == "entrar":
+                
+                player_ip, _ = addr
+                
+                player_id = enter_lobby(
+                    name=before_as_dict["nome"], 
+                    ip=player_ip,
+                    conn=conn
+                )
+                
+                broadcast_lobby()
+                
             message.append(before)
             
             ack = {
@@ -42,4 +63,4 @@ server.listen()
 with ThreadPoolExecutor(max_workers=10) as thread:
     while True:
         conn, addr = server.accept()
-        thread.submit(handle_connection, conn)
+        thread.submit(handle_connection, conn, addr)
